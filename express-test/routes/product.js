4f846566dff29f5f5ad85fd5339dbb1f657a1fd2
DoHyeon <whdies816@gmail.com>
2020-01-02 08:47:26 +0900

    Cors 적용, api 호출 적용

--- express-test/routes/product.js
+++ express-test/routes/product.js
@@ -4,21 +4,21 @@ var api = require('../../11st/out/index');
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  if (req.query.name) {
-    return callProduct(req.query.name, res);
-  }
-  return res.status(200).json({
-    success: true,
-    data: 'product name 이 없습니다.',
-  });
+    if (req.query.name) {
+        return callProduct(req.query.name, res);
+    }
+    return res.status(200).json({
+        success: true,
+        data: 'product name 이 없습니다.',
+    });
 });
 
 async function callProduct(productName, res) {
-  const result = await api.index(productName);
-  return res.status(200).json({
-    success: true,
-    data: result,
-  });
+    const result = await api.index(productName);
+    return res.status(200).json({
+        success: true,
+        data: result,
+    });
 }
 
 module.exports = router;
