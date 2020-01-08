dc4df54f568c404838c8080a310b933d89224cf2
DoHyeon <whdies816@gmail.com>
2020-01-08 16:41:49 +0900

    11번가 API 위치 변경

--- API/11st/services/request.service.ts
+++ API/11st/services/request.service.ts
@@ -1,6 +1,6 @@
 import * as rp from 'request-promise';
-import { APISite } from './option';
-import { SiteRequest } from '../interface/SiteRequest.interface';
+import { APISite } from '../product_search/src/option';
+import { SiteRequest } from '../product_search/interface/SiteRequest.interface';
 
 export const requestService = async (qs: SiteRequest) => {
     return rp.get({ url: APISite, qs, json: true, encoding: null });
--- express-for-typescript/src/product/callApp.service.ts
+++ express-for-typescript/src/product/callApp.service.ts
@@ -1,5 +1,5 @@
 import { ProductRequest } from './Interface/productRequest.interface';
-var api_11st = require('../../../API/11st/dist/src/index.js');
+var api_11st = require('../../../API/11st/dist/product_search/src/index.js');
 
 export const callApp = async (data: ProductRequest) => {
     try {
