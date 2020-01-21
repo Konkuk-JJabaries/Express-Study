da2cb1fd08f980a4ecd5b0581c258f7adff91b16
beoms <bm1478@naver.com>
2020-01-21 15:36:32 +0900

    change account info

---
+++ Query/data truncate.sql
@@ -0,0 +1,6 @@
+show databases;
+use joongna_test;
+show tables;
+
+select * from joongnatest;
+truncate joongnatest;
--- Query/table 생성.sql
+++ Query/table 생성.sql
@@ -1,62 +1,32 @@
 show databases;
+create database joongna_test;
 use joongna_test;
--- create table item_info;
--- drop table testdb;
 show tables;
 
-create table item_info (
-name varchar(50),
-price int,
-images varchar(100),
-origin_url varchar(100),
-site varchar(20),
-detail varchar(200),
-etc varchar(50)
-);
+-- create table item_info (
+-- name varchar(50),
+-- price int,
+-- images varchar(100),
+-- origin_url varchar(100),
+-- site varchar(20),
+-- detail varchar(200),
+-- etc varchar(50)
+-- );
 
-alter table item_info change detail id varchar(24);
-alter table item_info add category varchar(15);
-alter table item_info modify site varchar(20) default '' not null;
-alter table item_info modify name varchar(50) default '' not null primary key;
-select * from item_info;
-desc item_info;
-alter table item_info modify images varchar(200);
+-- alter table item_info change detail id varchar(24);
+-- alter table item_info add category varchar(15);
+-- alter table item_info modify site varchar(20) default '' not null;
+-- alter table item_info modify name varchar(50) default '' not null primary key;
+-- select * from item_info;
+-- desc item_info;
+-- alter table item_info modify images varchar(200);
 
 -- 11번가: siteName, productName, price, seller, images, rating, commentNum, productLink
 -- 중고나라: site, name, price, images, detail, origin_url, etc
 
-drop table joongnatest;
-create table joongnatest (
-id int not null auto_increment primary key,
-site varchar(20) not null,
-name varchar(50) not null,
-price int,
-images varchar(200),
-detail varchar(200),
-origin_url varchar(100),
-etc varchar(50)
-);
-alter table joongnatest modify images varchar(200);
-alter table joongnatest drop primary key;
-alter table joongnatest add id int not null auto_increment primary key;
-alter table joongnatest convert to charset utf8;
-desc joongnatest;
 
-create table ProductList (
-	id varchar(24) not null,
-    title varchar(50) not null primary key,
-    price int not null,
-    image varchar(100),
-    origin_url varchar(100) not null,
-    sitecode varchar(10) not null,
-    category varchar(15),
-    date date,
-    seller varchar(30),
-    status varchar(10)
-);
-
-alter table ProductList rename productList;
-alter table joongnaTest rename joongnatest;
-
-select * from productList;
-select * from joongnatest;
+-- alter table joongnatest modify images varchar(200);
+-- alter table joongnatest drop primary key;
+-- alter table joongnatest add id int not null auto_increment primary key;
+-- alter table joongnatest convert to charset utf8;
+-- desc joongnatest;
